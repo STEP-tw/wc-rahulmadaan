0b701cac93a617d4a7b744484e99d15a13e7874a
Rahul Madaan <44020002+rahulmadaan@users.noreply.github.com>
2018-12-26 15:31:46 +0530

    Splitted class into multiple functions in WordCount.js

--- src/wordCount.js
+++ src/wordCount.js
@@ -1,17 +1,16 @@
-class WordCount {
-
-    constructor(contents) {
-        this.contents = contents;
-    };
-    getLineCount() {
-        return this.contents.split('\n').length;
-    };
-    getByteCount() {
-        return this.contents.split('').length;
-    };
-    getWordCount() {
-        return this.contents.trim().split('\n').map(x=>x.trim().split(' ').length).reduce((a,b)=>a+b);
-    };
+const getLineCount = function (contents) {
+    return contents.split('\n').length;
 };
-exports.WordCount=WordCount;
+const getByteCount = function (contents) {
+    return contents.split('').length;
+};
+const getWordCount = function (contents) {
+    return contents.trim().split('\n').map(x => x.trim().split(' ').length).reduce((a, b) => a + b);
+};
+
+module.exports = {
+    getLineCount,
+    getByteCount,
+    getWordCount
+};
 
