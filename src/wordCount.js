7980818e87a8e8cd175e9f608410c67991b1bf8e
Rahul Madaan <44020002+rahulmadaan@users.noreply.github.com>
2018-12-26 15:38:06 +0530

    changed structure of wordCount library

--- src/wordCount.js
+++ src/wordCount.js
@@ -1,3 +1,9 @@
+const getCount = function (contents) {
+    const lineCount = getLineCount(contents);
+    const byteCount = getByteCount(contents);
+    const wordCount = getWordCount(contents);
+    return { lineCount, byteCount, wordCount };
+}
 const getLineCount = function (contents) {
     return contents.split('\n').length;
 };
@@ -9,8 +15,6 @@ const getWordCount = function (contents) {
 };
 
 module.exports = {
-    getLineCount,
-    getByteCount,
-    getWordCount
+    getCount
 };
 
