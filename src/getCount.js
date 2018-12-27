ddf2f5959e8aaae8957981016b0842f0cbc11048
Rahul Madaan <44020002+rahulmadaan@users.noreply.github.com>
2018-12-27 11:25:34 +0530

    fixed getWordCount

--- src/getCount.js
+++ src/getCount.js
@@ -1,10 +1,11 @@
+const splitByWords = content => content.split(/ |\n/).filter(x => x).length;
 const NEWLINE = "\n";
 const SPACE = " ";
+
 const getCount = function (contents) {
     const lineCount = getLineCount(contents);
     const byteCount = getByteCount(contents);
     const wordCount = getWordCount(contents);
-
     return { lineCount, byteCount, wordCount };
 }
 const getLineCount = function (contents) {
@@ -14,11 +15,10 @@ const getByteCount = function (contents) {
     return contents.split('').length;
 };
 const getWordCount = function (contents) {
-    const splitContent = contents.trim().split(NEWLINE);
-    const splitWordsCount = splitContent.map(x => x.trim().split(SPACE).length);
-    const wordCount = splitWordsCount.reduce((a, b) => a + b);
-    return wordCount;
-    // return contents.trim().split(NEWLINE).map(x => x.trim().split(SPACE).length).reduce((a, b) => a + b);
+    // const splitContent = contents.trim().split(NEWLINE);
+    //  const splitWordsCount = splitContent.map(x => x.trim().split(SPACE).length);
+    //  const wordCount = splitWordsCount.reduce((a, b) => a + b);
+    return splitByWords(contents)
 };
 
 module.exports = {
