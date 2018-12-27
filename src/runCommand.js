2d0df6f16abb817b18b051f07cb2196090acb824
Rahul Madaan <44020002+rahulmadaan@users.noreply.github.com>
2018-12-27 08:01:00 +0530

    removed unnecessary comments and simplified runCommand.js library

--- src/runCommand.js
+++ src/runCommand.js
@@ -16,35 +16,24 @@ const getDetails = function (fileName, fs) {
     const lineCount = counts.lineCount;
     return { fileName, contents, wordCount, byteCount, lineCount };
 };
-const formatter = function (counts) {
-    const { lineCount, byteCount, wordCount, fileName } = counts;
-    return [TAB, lineCount, TAB, wordCount, TAB, byteCount].join('') + ' ' + fileName;
-};
-const formatterForOneOption = function (count, fileName) {
-    return [TAB, count].join('') + ' ' + fileName;
-};
-const availableOptions = {
-    'l': 'lineCount',
-    'w': 'wordCount',
-    'c': 'byteCount'
-};
-
-const newFunction = function (fileName, option, fs) {
+const formatter = function (fileName, option, fs) {
     let output = "";
-    if (option == "") {
-        output = output + TAB + getDetails(fileName.join(''), fs)['lineCount'];
-        output = output + TAB + getDetails(fileName.join(''), fs)['wordCount'];
-        output = output + TAB + getDetails(fileName.join(''), fs)['byteCount'];
+    const data = getDetails(fileName.join(''), fs);
+
+    if (option.includes("l")) {
+        output = output + TAB + data["lineCount"];
     }
-    if (option.includes('l')) {
-        output = output + TAB + getDetails(fileName.join(''), fs)['lineCount'];
-    }
-    if (option.includes('w')) {
-        output = output + TAB + getDetails(fileName.join(''), fs)['wordCount'];
+    if (option.includes("w")) {
+        output = output + TAB + data["wordCount"];
 
     }
-    if (option.includes('c')) {
-        output = output + TAB + getDetails(fileName.join(''), fs)['byteCount'];
+    if (option.includes("c")) {
+        output = output + TAB + data["byteCount"];
+    }
+    if (option == "") {
+        output = output + TAB + data["lineCount"];
+        output = output + TAB + data["wordCount"];
+        output = output + TAB + data["byteCount"];
     }
     return output + ' ' + fileName;
 };
@@ -52,21 +41,7 @@ const newFunction = function (fileName, option, fs) {
 const wc = function (userArgs, fs) {
     const input = parseInput(userArgs);
     let { option, fileNames } = input;
-    //const data = getDetails(fileNames.join(''), fs);
-    if (option == undefined) {
-        option = "";
-    }
-    //const defaultCase = [undefined, "lcw", "lwc", "wcl", "wlc", "cwl", "clw"]; // undefined -> not given 
-
-    //   let result = '';
-    // if (defaultCase.includes(option)) {
-    //     return formatter(getDetails(fileNames.join(''), fs), fileNames.join(''));
-    // }
-    // if (option.includes("")) {
-    //     const inputOption = availableOptions[option];
-    //     return formatterForOneOption(data[inputOption], fileNames);
-    // }
-    return newFunction(fileNames, option, fs);
+    return formatter(fileNames, option, fs);
 };
 
 module.exports = {
