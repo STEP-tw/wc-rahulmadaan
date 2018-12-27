81bb2b946f48d51134d38366cbec6934279e767b
Rahul Madaan <44020002+rahulmadaan@users.noreply.github.com>
2018-12-27 14:25:37 +0530

    removed readline Sync from main

--- wc.js
+++ wc.js
@@ -1,9 +1,8 @@
 const { wc } = require('./src/runCommand.js');
 const fs = require('fs');
-const readLine = require('readline-sync');
+
 const main = function () {
   const userArgs = process.argv.slice(2);
-
   console.log(wc(userArgs, fs));
 }
 main();
